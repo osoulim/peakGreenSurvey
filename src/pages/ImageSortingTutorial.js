57138cafd4a85343196c911ab6ff49e81000ab2b
mamzi <mohammadreza.osouli@gmail.com>
2022-07-08 09:04:12 -0600

    update tutorials

--- src/pages/ImageSortingTutorial.js
+++ src/pages/ImageSortingTutorial.js
@@ -8,16 +8,16 @@ function ImageSortingTutorial() {
         <text style={{fontSize: 30}}>
           Welcome to first task tutorial.
         </text>
-        <p>In this task, you have some images at the top part of the page (about 5 images) such as bellow. </p>
+        <p>In this task, you have five images at the top of the page and need to rank them into three target positions.</p>
         <img src="/images/first_1.png" style={{borderWidth: 3, margin: 10}}/>
-        <p>And you have three targets at the bottom of the page such as bellow</p>
+        {/*<p>And you have three targets at the bottom of the page, such as below.</p>*/}
         <img src="/images/first_2.png" style={{borderWidth: 3, margin: 10}}/>
 
         <p>
-        Your task is to select top three brightest(greenest) images and drop them in the target. <br/>
-        The first target should contain the brightest image(s). The second target should contain the second brightest image(s) and so on.
+        Your task is to select the top three brightest(greenest) images and drop them in the target. <br/>
+        The first target should have the brightest image(s). The second target should contain the second brightest image(s).
         <br/>
-          After choosing the brighter image, just drag and drop the image inside the target. Such as bellow.
+          After choosing the brighter image, drag and drop the image inside the target (see below).
         </p>
 
         <img src="/images/first_3.gif" style={{borderWidth: 3, margin: 10}}/>
@@ -26,8 +26,8 @@ function ImageSortingTutorial() {
 
         <img src="/images/first_4.gif" style={{borderWidth: 3, margin: 10}}/>
 
-        <p>Don't worry if you can not choose between two or more images, you can have more than one image in each target. <br/>
-        So just easily drag the image into the target like bellow.
+        <p>
+          If you can not rank two or more images, you can assign more than one image to each target position by simply dragging multiple images into the same target position (see below).
         </p>
 
         <img src="/images/first_5.gif" style={{borderWidth: 3, margin: 10}}/>
@@ -35,7 +35,7 @@ function ImageSortingTutorial() {
         <p> It was all you needed to learn before starting the task. </p>
 
         <div style={{paddingTop: 20, paddingBottom: 150}}>
-          <button onClick={() => navigate("/image-sorting/1")} style={{backgroundColor: "#ECB365", padding: 10, color: "#000", borderRadius: 10}}> Go to the first task -></button>
+          <button onClick={() => navigate("/image-sorting/1")} style={{backgroundColor: "#ECB365", padding: 10, color: "#000", borderRadius: 10}}> Let us start the tasts -></button>
         </div>
 
       </div>
